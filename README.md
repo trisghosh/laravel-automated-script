0e20b21f6605e9b7f5ed6cb3b6788ac2f8c92788
tristup <tristup@itobuz.com>
2018-01-02 16:34:13 +0530

    Auth package add with migration options

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ Clone the repo or download the install.sh file and keep into your root folder i.
 Go to terminal and run the install.sh file [ sh install.sh ]
 It will ask the project name, and on the given project name it will create a folder and install laravel. 
 
+Auth : Auth package is integrated php artisan make:auth
+
+Migrate DB : php artisan migrate
+
 Main Focus : is to install the laravel with all the requirement it need minimum to run in a single shot.
--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 echo 'Enter Your Project Name : '
 read project_name
+if [ "$project_name" = "" ]; then
+	project_name="blog"
+fi;
 #Into Project folder
 composer create-project --prefer-dist laravel/laravel $project_name
 cd $project_name
@@ -7,4 +10,17 @@ cp .env.example .env
 sudo chmod -R 0777 storage/
 sudo chmod -R 0777 bootstrap/cache/
 php artisan key:generate
+
+echo 'Do you need Auth Scaffolding : Y/N'
+read authr
+if [ "$authr" = "Y" ]; then
+	php artisan make:auth
+
+	echo 'Do you need Migrate the DB : Y/N'
+	read dbmig
+
+	if [ "$dbmig" = "Y" ]; then
+		php artisan migrate
+	fi;
+fi;
 
